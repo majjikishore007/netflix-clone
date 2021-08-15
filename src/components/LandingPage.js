a81369ce087ac22126d59b7369905836be9d295e
majjikishore007 <majjikishore031@gmail.com>
2021-08-15 21:29:18 +0530

    added image sliders

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/landingpage.css";
 import Background from "./Background";
-const LandingPage = (props) => {
+import ReactPlayer from "react-player";
+const LandingPage = () => {
   return (
     <div>
       <Background>
@@ -39,10 +40,86 @@ const LandingPage = (props) => {
             </div>
             <div className='vedio-content'>
               <img src='/images/tv.png' alt='' />
-              {console.log("hello")}
               <video width='750' height='500' autoPlay>
                 <source src='/vedioes/video-tv-in-0819.m4v' type='video/mp4' />
               </video>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='container'>
+          <div className='split'>
+            <div className='vedio-content'>
+              <img src='/images/mobile-0819.jpg' alt='' />
+              <div className='animation'>
+                <div className='animation-img'>
+                  <img src='/images/boxshot.png' alt='' />
+                </div>
+                <div className='animation-content'>
+                  <div style={{ fontWeight: "bold" }}>Stranger Things</div>
+                  <div style={{ color: "blue" }}>Downloading...</div>
+                </div>
+                <div>
+                  <img
+                    style={{ width: "100%", height: "100%" }}
+                    src='/images/download-icon.gif'
+                    alt=''
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='split-content'>
+              <h2>Download your shows to watch offline.</h2>
+              <p>
+                Save your favourites easily and always have something to watch.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='container'>
+          <div className='split'>
+            <div className='split-content'>
+              <h2>Watch everywhere.</h2>
+              <p>
+                Stream unlimited movies and TV shows on your phone, tablet,
+                laptop, and TV.
+              </p>
+            </div>
+            <div className='vedio-content'>
+              <img src='/images/device-pile-in.png' alt='' />
+              <video
+                style={{
+                  width: "60%",
+                  height: "56%",
+                  top: "0px",
+                  left: "19%",
+                }}
+                autoPlay
+              >
+                <source src='/vedioes/video-devices-in.m4v' type='video/mp4' />
+              </video>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className='container'>
+          <div className='split'>
+            <div className='vedio-content'>
+              <img
+                src='/images/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png'
+                alt=''
+              />
+            </div>
+            <div className='split-content'>
+              <h2>Create profiles for children.</h2>
+              <p>
+                Send children on adventures with their favourite characters in a
+                space made just for them—free with your membership.
+              </p>
             </div>
           </div>
         </div>
