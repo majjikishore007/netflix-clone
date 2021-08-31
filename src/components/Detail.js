186df56ab5e2dccdc915c9393073725e5eec896e
majjikishore007 <majjikishore031@gmail.com>
2021-08-31 16:58:48 +0530

    updated with detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { getMovieById, getVedioKey } from "../data/Movies/tmdb_helper";
+import Navbar from "./Navbar";
 import Spinner from "./Spinner";
 import Video from "./Video";
 const Detail = () => {
@@ -9,6 +10,7 @@ const Detail = () => {
   const [key, setKey] = useState("");
   const [movie, setMovie] = useState({});
   const [loadSpinner, setLoadSpinner] = useState(true);
+  const [show, setShow] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       setLoadSpinner(false);
@@ -25,10 +27,28 @@ const Detail = () => {
     console.log("movie", data);
     setMovie(data);
   };
+  const handleT = () => {
+    setShow(!show);
+  };
+  const showTrailer = () => {
+    console.log("show");
+    return (
+      <div className='container'>
+        <VideoContainer>
+          <CloseButton onClick={handleT}>
+            <span>x</span>
+          </CloseButton>
+          <Video width={"600px"} height={"400px"} vediokey={key}></Video>
+        </VideoContainer>
+      </div>
+    );
+  };
   const getDetalData = () => {
     return (
       <>
+        <Navbar></Navbar>
         <Container>
+          {show && showTrailer()}
           {key && (
             <>
               <Background>
@@ -38,14 +58,14 @@ const Detail = () => {
               </Background>
               <ImageTitle></ImageTitle>
               <VideoContainer>
-                <Video width={"640px"} height={"480px"} vediokey={key}></Video>
+                {/* <Video width={"640px"} height={"480px"} vediokey={key}></Video> */}
               </VideoContainer>
               <Controls>
                 <PlayButton>
                   <img src='/images/play-icon-black.png' alt='' />
                   <span>PLAY</span>
                 </PlayButton>
-                <TrailerButton>
+                <TrailerButton onClick={handleT}>
                   <img src='/images/play-icon-white.png' alt='' />
                   <span>Trailer</span>
                 </TrailerButton>
@@ -55,6 +75,9 @@ const Detail = () => {
               </Controls>
               <SubTitle>{movie.original_title}</SubTitle>
               <Description>{movie.overview}</Description>
+              <div>
+                <h3>{movie.genre}</h3>
+              </div>
             </>
           )}
         </Container>
@@ -67,7 +90,9 @@ const Detail = () => {
 export default Detail;
 const VideoContainer = styled.div`
   position: absolute;
-  left: 0px;
+  left: 70%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 `;
 const Container = styled.div`
   min-height: calc(100vh - 70px);
@@ -83,8 +108,7 @@ const Background = styled.div`
   bottom: 0;
   right: 0;
   z-index: -1;
-  opacity: 0.8;
-
+  opacity: 0.3;
   img {
     width: 100%;
     height: 100%;
@@ -154,13 +178,14 @@ const AddButton = styled.button`
     color: white;
   }
 `;
-
+const CloseButton = styled(AddButton)``;
 const SubTitle = styled.div`
   z-index: 2;
   color: rgb(249, 249, 249);
-  font-size: 15px;
+  font-size: 20px;
   min-height: 20px;
   margin-top: 26px;
+  font-weight: 600;
 `;
 
 const Description = styled.div`
